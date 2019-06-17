be1e00fb147babe0583cfd49e448d3c540fb113c
RobertWoodhouse <47441722+RobertWoodhouse@users.noreply.github.com>
2019-06-17 06:53:57 +0100

    Text File Reader - Assessment

---
+++ TxtFileReader_RWoodhouse/TxtFileReader_RWoodhouse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace TxtFileReader_RWoodhouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TxtFileReader(@"mytest.txt");
+        }
+
+        static void TxtFileReader(string filePath)
+        {
+            try
+            {
+                string[] line = File.ReadAllLines(filePath);
+                Console.WriteLine(line[0]);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Error: \"mytest.txt\" file not found");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Error: Array line number out of range");
+            }
+        }
+    }
+}
